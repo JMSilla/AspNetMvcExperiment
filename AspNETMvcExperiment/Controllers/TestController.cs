7588eee1a42f3b3459033673282cf7328ad64676
José María Silla Martínez <josilma1@gmail.com>
2017-07-02 11:45:35 +0100

    New test controller with two action methods. In the first a form is displayed, and when it is submitted the second method is executed, returning to a view that shows the form values.

---
+++ AspNETMvcExperiment/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace AspNETMvcExperiment.Controllers
+{
+	public class TestController : Controller
+	{
+		public ActionResult Create() 
+		{
+			return View ();
+		}
+
+		[HttpPost]
+		public ActionResult Create(string name, string description) 
+		{
+			ViewBag.name = name;
+			ViewBag.description = description;
+
+			return View ("Result");
+		}
+	}
+}
